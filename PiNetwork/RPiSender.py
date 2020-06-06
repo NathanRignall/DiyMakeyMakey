33196dc437fd8efb15c5e192a37d62ab744aac76
Nathan Rignall <nathan.rignall@rignall.local>
2020-06-06 18:40:35 +0100

    master

--- PiNetwork/RPiSender.py
+++ PiNetwork/RPiSender.py
@@ -1,8 +1,25 @@
 import Network as net
+import RPi.GPIO as GPIO
 
+keys = [0,0,0]
+
+def key0(channel):
+    if GPIO.input(channel) == GPIO.HIGH:
+        key[0] = 1
+    else:
+        key[0] = 0
 try:
     net.sendInitializeSocket(port=9000, client="192.168.54.3")
-    net.sendData("Test!")
+    net.sendData("Starup")
+    GPIO.setwarnings(False) # Ignore warning for now
+    GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+    GPIO.setup(10, GPIO.IN, GPIO.BOTH, callback=key0)
+    GPIO.setup(11, GPIO.IN, GPIO.BOTH, callback=key1)
+    GPIO.setup(12, GPIO.IN, GPIO.BOTH, callback=key2)
+    while True:
+        net.sendData(str(keys))
+        print(keys)
+        time.sleep(0.05)
     net.sendEndSocket()
 except:
     net.sendEndSocket()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DiyMakeyMakey
+
+<b>Simple programs to turn devices into DIY Makey Makey devices</b>
+
+<p>Includes</p>
+<li>Microbit - Serial</li>
+<li>Raspberry Pi - Network</li>
