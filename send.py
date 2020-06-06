0ed25feea5b483dd6008858a0b8fbc0549892280
Nathan Rignall <nathan.rignall@rignall.local>
2020-06-06 18:00:02 +0100

    master

--- send.py
+++ send.py
@@ -1,22 +1,21 @@
 import socket
 
-host = ''
-port = 8989
-locaddr = (host,port) 
-client = '192.168.0.1'
-
-# Create a UDP socket
-
-try:
-    msg = 'test'
-    msg = msg.encode(encoding="utf-8") 
-    sent = sock.sendto(msg, tello_address)
-except KeyboardInterrupt:
-    print ('\n . . .\n')
-    sock.close()  
-
-def initializeSocket():
+def initializeSocket(client = '192.168.0.1', port = 8989):
     global sock, tello_address
+    host = ''
+    locaddr = (host,port) 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     tello_address = (client, port)
     sock.bind(locaddr)
+    print("socket opened")
+
+def endSocket():
+    global sock, tello_address
+    sock.close()
+    print("socket closed")
+
+def sendData(data):
+    global sock, tello_address
+    msg = data.encode(encoding="utf-8")
+    sock.sendto(msg, tello_address)
+    print("sent: ", msg)
