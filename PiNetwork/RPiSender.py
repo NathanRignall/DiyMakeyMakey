bcc6b08ee285ed54d30ce4f7dfcf3697f5867bf9
Riggers03 <nathanrignall@icloud.com>
2020-06-07 00:28:48 +0100

    master

--- PiNetwork/RPiSender.py
+++ PiNetwork/RPiSender.py
@@ -18,18 +18,16 @@ def key2(channel):
         keys[2] = 1
     else:
         keys[2] = 0
-try:
-    net.sendInitializeSocket(port=9000, client="192.168.54.3")
-    net.sendData("Starup")
-    GPIO.setwarnings(False) # Ignore warning for now
-    GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-    GPIO.setup(10, GPIO.IN, GPIO.BOTH, callback=key0)
-    GPIO.setup(11, GPIO.IN, GPIO.BOTH, callback=key1)
-    GPIO.setup(12, GPIO.IN, GPIO.BOTH, callback=key2)
-    while True:
-        net.sendData(str(keys))
-        print(keys)
-        time.sleep(0.05)
-    net.sendEndSocket()
-except:
-    net.sendEndSocket()
+
+net.sendInitializeSocket(port=9000, client="192.168.54.3")
+net.sendData("Starup")
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(11, GPIO.IN, GPIO.BOTH, callback=key0)
+GPIO.setup(13, GPIO.IN, GPIO.BOTH, callback=key1)
+GPIO.setup(15, GPIO.IN, GPIO.BOTH, callback=key2)
+while True:
+    net.sendData(str(keys))
+    print(keys)
+    time.sleep(0.05)
+net.sendEndSocket()
