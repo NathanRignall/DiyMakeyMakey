1d17b977740ed4ec1b1c81571239421f012fcf6d
Riggers03 <nathanrignall@icloud.com>
2020-06-06 23:22:26 +0100

    master

--- MicroBitSerial/MicroSender.py
+++ MicroBitSerial/MicroSender.py
@@ -1,1 +1,26 @@
-#to enter
+# Write your code here :-)
+from microbit import *
+
+pins = [0, 0, 0]
+
+uart.init(115200)
+
+while True:
+    display.set_pixel(4, 4, 9)
+    if pin0.is_touched():
+        pins[0] = 1
+        display.set_pixel(1, 0, 9)
+    if pin1.is_touched():
+        pins[1] = 1
+        display.set_pixel(2, 0, 9)
+    if pin2.is_touched():
+        pins[2] = 1
+        display.set_pixel(3, 0, 9)
+    burffer = str(pins) + "\n"
+    uart.write(burffer)
+    sleep(100)
+    display.set_pixel(4, 4, 0)
+    pins = [0, 0, 0]
+    display.set_pixel(1, 0, 0)
+    display.set_pixel(2, 0, 0)
+    display.set_pixel(3, 0, 0)
